0a962b9ad41ea20b72ef76e021ff58eaec8735fa
jcorredorc <javier.adolfo@gmail.com>
2021-08-04 22:31:32 +0000

    feat: a single goal to navigation stack and the test_navigation.sh script working ok

--- scripts/test_navigation.sh
+++ scripts/test_navigation.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:="/home/workspace/catkin_ws/src/home_service_robot/worlds/world_mapping_features.world"" &
+sleep 10
+xterm  -e  " export TURTLEBOT_GAZEBO_MAP_FILE=/home/workspace/gmapping_01.yaml ; roslaunch turtlebot_gazebo amcl_demo.launch" & 
 sleep 8
-xterm  -e  " export TURTLEBOT_MAP_FILE=/home/workspace/gmapping_01.yaml ; roslaunch turtlebot_navigation amcl_demo.launch" & 
-sleep 5
 xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch"
