6c4839fda835896fda3612a00eb9dbdba2a624de
jcorredorc <javier.adolfo@gmail.com>
2021-08-05 23:28:46 +0000

    fix: adding the map to repo
    To Do: debug the node single_goal.cpp

--- src/single_goal.cpp
+++ src/single_goal.cpp
@@ -7,7 +7,7 @@ typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseCl
 
 int main(int argc, char** argv){
   // Initialize the simple_navigation_goals node
-  ros::init(argc, argv, "simple_navigation_goals");
+  ros::init(argc, argv, "single_goal");
 
   //tell the action client that we want to spin a thread by default
   MoveBaseClient ac("move_base", true);
@@ -20,12 +20,18 @@ int main(int argc, char** argv){
   move_base_msgs::MoveBaseGoal goal;
 
   // set up the frame parameters
-  goal.target_pose.header.frame_id = "base_link";
+  goal.target_pose.header.frame_id = "map";
   goal.target_pose.header.stamp = ros::Time::now();
 
   // Define a position and orientation for the robot to reach
-  goal.target_pose.pose.position.x = 1.0;
-  goal.target_pose.pose.orientation.w = 1.0;
+  goal.target_pose.pose.position.x = 4.1003279686;
+  goal.target_pose.pose.position.y = 3.93584132195;
+  goal.target_pose.pose.position.z = 0.0;
+
+  goal.target_pose.pose.orientation.x = 0.0;
+  goal.target_pose.pose.orientation.y = 0.0;
+  goal.target_pose.pose.orientation.z = 0.999754962967;
+  goal.target_pose.pose.orientation.w = -0.0221362603563;
 
    // Send the goal position and orientation for the robot to reach
   ROS_INFO("Sending goal");
@@ -36,9 +42,9 @@ int main(int argc, char** argv){
 
   // Check if the robot reached its goal
   if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-    ROS_INFO("Hooray, the base moved 1 meter forward");
+    ROS_INFO("Hooray, the base moved to goal");
   else
-    ROS_INFO("The base failed to move forward 1 meter for some reason");
+    ROS_INFO("The base failed to move to goal");
 
   return 0;
 }
