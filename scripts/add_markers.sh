d83f3b9f9f1d302e8beda2fdb2bc25c76ad9e625
jcorredorc <javier.adolfo@gmail.com>
2021-08-06 21:23:32 +0000

    fix: The add_markers works acconding to the request
    By time appear a cube marker for 5 sec. and therefore disapear to repeat the secuence with the dropoff task

---
+++ scripts/add_markers.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+xterm  -e  " roslaunch home_service_robot turtlebot_world.launch " &
+sleep 10
+xterm  -e  " roslaunch home_service_robot amcl_demo.launch" & 
+sleep 8
+xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 10
+xterm  -e  " rosrun home_service_robot add_markers_node"
